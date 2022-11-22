d28c201a2e5b5b900848c859981aae96980d5d7d
kkuchta <kkuchta@griddynamics.com>
2022-11-22 02:42:40 +0100

    fixed reading from stdin

--- main.c
+++ main.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
-#include<ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-#define  MAX_SIZE 1000
+#define MAX_LINE_SIZE 100
+#define MAX_LINES 1000
 
 int main() {
-    double numbers[MAX_SIZE];
-    for (int i = 0; i < sizeof(numbers) / sizeof(numbers[0]); ++i) {
-        numbers[i] = 0;
+    long numbers[MAX_LINES] = {0};
+    char line[MAX_LINE_SIZE] = {0};
+    int counter = 0;
+    while (fgets(line, MAX_LINE_SIZE, stdin) != NULL && counter < MAX_LINES) {
+        numbers[counter] = strtol(line, NULL, 10);
+        counter++;
     }
-    unsigned int counter = 0;
-    int character;
-    while ((character = getchar() != EOF)) {
-        if (counter < 1000 && isdigit(character)) {
-            numbers[counter] = character;
-            counter++;
-        } else {
-            printf("Given character is not a");
-            continue;
-        }
+    double arithmetic_average = 0;
+
+    for (int i = 0; i < counter; ++i) {
+        arithmetic_average += (double) numbers[i];
     }
-    return 69;
+    arithmetic_average /= counter;
+    printf("arithmetic average %lf\n", arithmetic_average);
+    double standard_deviation = 0;
+    for (int i = 0; i < counter; ++i) {
+        standard_deviation += pow(((double) numbers[i] - arithmetic_average), 2);
+    }
+    standard_deviation /= counter;
+    standard_deviation = sqrt(standard_deviation);
+
+    printf("standard deviation %lf \n", standard_deviation);
 }
